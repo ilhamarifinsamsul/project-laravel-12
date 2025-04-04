e839cc64b811831ce57ece42fcc5e6a7e5286bc6
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-04-04 10:28:00 +0700

    templating dashboard admin

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright Ilham Arifin &copy; {{ date('Y') }}</span>
+        </div>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dashboard', function(){
+    return view('admin.dashboard.index');
+})->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
